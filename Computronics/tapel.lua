d8af650c09fb662b28f7eee0fb58021e34de7f27
Vexatos <Vexatos@users.noreply.github.com>
2014-04-18 10:16:20 +0200

    Corrected formatting

--- Computronics/tapel.lua
+++ Computronics/tapel.lua
@@ -1,10 +1,11 @@
 --tapel, labels the tape(doesn't take spaces yet, sorry)
 local component = require("component")
 local tape = component.tape_drive
- 
+
 local args = {...}
 if #args < 1 then
-        print("Usage: tapel {label}")
+  print("Usage:")
+  print("tapel <label>")
 else
-        tape.setLabel(args[1])
+  tape.setLabel(args[1])
 end
