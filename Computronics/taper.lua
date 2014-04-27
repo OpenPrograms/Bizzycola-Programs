df494f4bbc42ea2bb591a6eb990d8d2a2d9ee18c
Vexatos <Vexatos@users.noreply.github.com>
2014-04-27 15:39:37 +0200

    Update taper.lua

--- Computronics/taper.lua
+++ Computronics/taper.lua
@@ -2,6 +2,12 @@
 --Authors: Bizzycola and Vexatos
 
 local component = require("component")
+
+if not component.isAvailable("tape_drive") then
+  io.stderr:write("This program requires a tape drive to run.")
+  return
+end
+
 local tape = component.tape_drive
 
 if not tape.isReady() then
