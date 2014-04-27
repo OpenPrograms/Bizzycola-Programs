bbb6bef2338bad43d6feb8cb7f24c3328ac58ca9
Vexatos <Vexatos@users.noreply.github.com>
2014-04-27 15:38:56 +0200

    Update tapel.lua

--- Computronics/tapel.lua
+++ Computronics/tapel.lua
@@ -1,8 +1,19 @@
 --tapel, labels the tape(doesn't take spaces yet, sorry)
---Author: Bizzycola
+--Author: Bizzycola and Vexatos
 
 local component = require("component")
+
+if not component.isAvailable("tape_drive") then
+  io.stderr:write("This program requires a tape drive to run.")
+  return
+end
+
 local tape = component.tape_drive
+
+if not tape.isReady() then
+  io.stderr:write("The tape drive does not contain a tape.")
+  return
+end
 
 local args = {...}
 if #args < 1 then
