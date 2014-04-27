a184f17169ff3fff1272a40d35110c02dbafb1a2
Vexatos <Vexatos@users.noreply.github.com>
2014-04-27 14:44:51 +0200

    Added support to get file from a URL

--- Computronics/tapew.lua
+++ Computronics/tapew.lua
@@ -1,12 +1,15 @@
 --tapew, use this to write to the tape
+--Authors: Bizzycola and Vexatos
+
 local component = require("component")
 local shell = require("shell")
-local args = shell.parse(...)
+local args,options = shell.parse(...)
 
 if #args<=0 then
   print("tapew, use this to write a file to the tape")
   print("Usage:")
-  print("tapew <path/of/audio/file>")
+  print("'tapew <path/of/audio/file>' to write from a file")
+  print("'tapew -o <URL>' to write from a URL")
   return
 end
 
@@ -16,17 +19,70 @@ if not component.isAvailable("tape_drive") then
 end
 
 local tape = component.tape_drive
-
-local file = io.open(shell.resolve(args[1]), "rb")
 local block = 1024
 
-print("Writing...")
-
 while true do
-local bytes = file:read(block)
-if not bytes then break end
-tape.write(bytes)
+  if tape.seek(-1000000) < 1 then
+    break
+  end
 end
-file:close()
+tape.stop()
 
-print("Done.")
+if options.o then
+
+  local url = string.gsub(args[1],"https?://","",1)
+  local domain = string.gsub(url,"/.*","",1)
+  print("Domain: "..domain)
+  local path = string.gsub(url,".-/","/",1)
+  print("Path: "..path)
+
+  if not component.isAvailable("internet") then
+  io.stderr:write("This program requires an internet card to run.")
+  return
+  end
+
+  local internet = require("internet")
+  local file = internet.open(domain, 80)
+  local start = false
+  
+  print("Writing...")
+
+  file:write("GET "..path.." HTTP/1.1\r\nHost: "..domain.."\r\n\r\n")
+  repeat
+    local bytes = file:read(block)
+    if string.find(bytes,"\r\n\r\n") then
+    bytes = string.gsub(bytes,".*\r\n\r\n","",1)
+    if not bytes then break end
+      tape.write(bytes)
+      start = true
+    end
+  until start == true
+
+  while true do
+    local bytes = file:read(block)
+    if not bytes then break end
+    tape.write(bytes)
+  end
+  file:close()
+
+  print("Done.")
+  
+else
+  local path = shell.resolve(args[1])
+  print("Path: "..path)
+  local file,msg = io.open(shell.resolve(path), "rb")
+  if not file then
+    print("Error: "..msg)
+    return
+  end
+  print("Writing...")
+
+  while true do
+    local bytes = file:read(block)
+    if not bytes then break end
+    tape.write(bytes)
+  end
+  file:close()
+
+  print("Done.")
+end
