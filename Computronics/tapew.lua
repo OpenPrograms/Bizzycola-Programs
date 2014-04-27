44e76fe9a80f43bf3944a7cfc6f6c94b08a5a8db
Vexatos <Vexatos@users.noreply.github.com>
2014-04-27 20:04:15 +0200

    Added more things
    @gamax92 to the rescue

--- Computronics/tapew.lua
+++ Computronics/tapew.lua
@@ -2,6 +2,7 @@
 --Authors: Bizzycola and Vexatos
 
 local component = require("component")
+local fs = require("filesystem")
 local shell = require("shell")
 local term = require("term")
 local args,options = shell.parse(...)
@@ -43,8 +44,6 @@ else
 end
 --End of gamax92's part
 
-local block = 1024
-
 if not tape.isReady() then
   io.stderr:write("The tape drive does not contain a tape.")
   return
@@ -55,7 +54,9 @@ tape.seek(-tape.getSize())
 tape.stop() --Just making sure
 
 local file,msg
+local block = 1024 --How much to read at a time
 local bytery = 0 --For the progress indicator
+local filesize = block
 if options.o then
 
   local url = string.gsub(args[1],"https?://","",1)
@@ -73,7 +74,8 @@ if options.o then
   file = internet.open(domain, 80)
   file:setTimeout(10)
   local start = false
-  
+  local _,y = term.getCursor()
+
   print("Writing...")
 
   file:write("GET "..path.." HTTP/1.1\r\nHost: "..domain.."\r\nConnection: close\r\n\r\n")
@@ -82,7 +84,12 @@ if options.o then
     if string.find(bytes,"\r\n\r\n") then
       bytes = string.gsub(bytes,".-\r\n\r\n","",1)
       if not bytes then break end
+      if string.match(bytes,"Content%-Length: (.-)\r\n") then
+      filesize = tonumber(string.match(bytes,"Content%-Length: (%d-)\r\n"))
+      end
+      term.setCursor(1,y)
       bytery = bytery + #bytes
+      term.write("Read "..tostring(bytery).." bytes...")
       tape.write(bytes)
       start = true
     elseif string.find(bytes,"\r\n?$") then
@@ -92,7 +99,9 @@ if options.o then
       if string.find(match,"\r\n\r\n") then
         bytes = string.gsub(match,".-\r\n\r\n","",1)
         if not bytes then break end
+        term.setCursor(1,y)
         bytery = bytery + #bytes
+        term.write("Read "..tostring(bytery).." bytes...")
         tape.write(bytes)
         start = true
       end
@@ -100,22 +109,27 @@ if options.o then
   until start == true
 else
   local path = shell.resolve(args[1])
+  filesize = fs.size(path)
   print("Path: "..path)
   file,msg = io.open(shell.resolve(path), "rb")
   if not file then
-    print("Error: "..msg)
+    io.stderr:write("Error: "..msg)
     return
   end
   print("Writing...")
 end
 
-local _,y = term.getCursor()
+if filesize > tape.getSize() then
+  io.stderr:write("Error: File is too large for tape, shortening file")
+  filesize = tape.getSize()
+end
+
 while true do
-  local bytes = file:read(block)
-  term.setCursor(1,y)
+  local bytes = file:read(math.min(block,filesize-bytery))
   if (not bytes) or bytes == "" then break end
+  term.setCursor(1,y)
   bytery = bytery + #bytes
-  term.write("Read "..tostring(bytery).." bytes...")
+  term.write("Read "..tostring(bytery).." of "..tostring(filesize).." bytes...")
   tape.write(bytes)
 end
 file:close()
